bbfadaebee118c13a31a587daabce4da5ab3d628
PedrooL77 <129890251+PedrooL77@users.noreply.github.com>
2023-04-05 11:53:10 -0300

    alteraçao num2

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ document.getElementById( "botao" ).addEventListener( "click" , shownome )
 
 document.getElementById( "num1" ).addEventListener( "click" ,shownumber)
 
+document.getElementById( "num2" ).addEventListener( "click" ,shownumber)
+
 function showcolor()
 {
     document.body.style.backgroundColor = "red";
@@ -14,6 +16,13 @@ function shownome()
     let usuario = document.getElementById( "nome" ).value;
     document.getElementById( "mensagem" ).innerHTML = "Meu nome é:" + usuario
     document.getElementById( "nome" ).value = "";
+}
+
+function shownumber()
+{
+    let numero = document.getElementById( "number" ).value;
+    document.getElementById( "resultado1").innerHTML = "Somar" + numero
+    document.getElementById( "number").value = ""
 }
 
 function shownumber()
