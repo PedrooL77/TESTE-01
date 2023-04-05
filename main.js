b683a52e63c1fc667facec36323c6c2dabcd2d25
PedrooL77 <129890251+PedrooL77@users.noreply.github.com>
2023-04-05 11:26:44 -0300

    alteraçao js

--- main.js
+++ main.js
@@ -11,4 +11,5 @@ function shownome()
 {
     let usuario = document.getElementById( "nome" ).value;
     document.getElementById( "mensagem" ).innerText = "Meu nome é:" + usuario
+    document.getElementById( "nome" ).value = "";
 }
