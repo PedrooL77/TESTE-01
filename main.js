2051e17fc24438b289d45b3b688b7285464a5292
PedrooL77 <129890251+PedrooL77@users.noreply.github.com>
2023-04-05 11:52:11 -0300

    incluindo soma html e js

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ document.addEventListener( "click" , showcolor );
 
 document.getElementById( "botao" ).addEventListener( "click" , shownome )
 
+document.getElementById( "num1" ).addEventListener( "click" ,shownumber)
+
 function showcolor()
 {
     document.body.style.backgroundColor = "red";
@@ -12,4 +14,11 @@ function shownome()
     let usuario = document.getElementById( "nome" ).value;
     document.getElementById( "mensagem" ).innerHTML = "Meu nome é:" + usuario
     document.getElementById( "nome" ).value = "";
+}
+
+function shownumber()
+{
+    let numero = document.getElementById( "number" ).value;
+    document.getElementById( "resultado1").innerHTML = "Somar" + numero
+    document.getElementById( "number").value = ""
 }
